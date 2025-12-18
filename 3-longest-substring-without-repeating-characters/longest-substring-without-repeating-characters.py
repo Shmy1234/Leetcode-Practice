e10eabe7aac0700b426609214992f800bea22d5a
Shmy <82426113+Shmy1234@users.noreply.github.com>
2025-12-17 21:55:32 -0500

    Time: 27 ms (21.82%) | Memory: 17.8 MB (81.56%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,13 +5,11 @@ class Solution:
         substring = set()
         while j <= len(s) - 1:
             if s[j] in substring:
-                longest = max(longest, len(substring))
                 substring.remove(s[i])
                 i += 1
             else:
                 substring.add(s[j])
-                longest = max(longest, len(substring))
                 j+=1
-
+            longest = max(longest, len(substring))
         return longest
             
