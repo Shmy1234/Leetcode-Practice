cd9d2f154a40422d3ddc8ae278ed537559475977
Shmy <82426113+Shmy1234@users.noreply.github.com>
2025-12-21 21:55:09 -0500

    Time: 20 ms (25.40%) | Memory: 21.7 MB (36.71%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -6,5 +6,5 @@ class Solution:
             for char in word:
                 l[ord(char) - ord('a')] += 1 
             d[tuple(l)] = [word] + d.get(tuple(l), [])
-        l = list(d.values())
+            
         return list(d.values())
