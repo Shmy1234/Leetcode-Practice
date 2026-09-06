78c96a83b6111db1b61d6f600891025da0ab6b65
Shmy <82426113+Shmy1234@users.noreply.github.com>
2026-09-05 23:38:18 -0400

    Time: 19 ms (19.74%) | Memory: 23.8 MB (26.58%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = {}
-        for s in strs:
+        for s in strs: 
             l = [0]*26
             for c in s: 
-                l[ord(c) - ord("a")] +=1 
+                l[ord(c) - ord("a")] +=1
             
-            if tuple(l) in d:
-                d[tuple(l)].append(s)
-            else:
-                d[tuple(l)] = [s]
-
+            d[tuple(l)] = [s] + d.get(tuple(l), [])
+        
+        if d == {}:
+            return []
         return list(d.values())
