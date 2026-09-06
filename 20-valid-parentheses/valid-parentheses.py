25c37e4bb6e4f59797bb0095f6e82b0f3d60659e
Shmy <82426113+Shmy1234@users.noreply.github.com>
2026-09-06 04:35:17 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        tracker = {')': '(', ']': '[', '}': '{'}
-        lst = []
-        for char in s:
-            if char in ")]}":
-                if len(lst) != 0 and lst[-1] == tracker[char]:
-                    lst.pop()
-                else: 
+        d = {')':'(', ']':'[', '}':'{'}
+        stack = []
+        for c in s: 
+            if c in "({[":
+                stack.append(c)
+            elif c in d:
+                if stack and stack.pop() == d[c]:
+                    continue
+                else:
                     return False
-            elif char in "([{":
-                lst.append(char)
         
-        return True if len(lst) == 0 else False
+        return stack == []
+        
